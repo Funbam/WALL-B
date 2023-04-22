d982ecb91cc89ee30e7f2b243a8f7f67064f057d
Ryan Ngoon <ryan.ngoon@gmail.com>
2023-04-22 16:03:31 -0700

    add spark effect

--- WALL-B/Assets/Scripts/Respawn.cs
+++ WALL-B/Assets/Scripts/Respawn.cs
@@ -6,11 +6,13 @@ using Cinemachine;
 public class Respawn : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject activateEffect;
     [SerializeField] private Transform spawnLocation;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
         RespawnManager.Instance.currentRespawn = this;
+        Instantiate(activateEffect, transform.position, Quaternion.identity);
     }
 
     public void RespawnPlayer()
