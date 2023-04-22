c788c317345390c195b99a7b6336e925eaec3082
Funbam <bambamtheram@gmail.com>
2023-04-22 10:47:52 -0700

    Leaderboard

---
+++ WALL-B/Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> names;
+    [SerializeField] private List<TextMeshProUGUI> scores;
+    [SerializeField] private int maxUsernameLen;
+    private string publicLeaderboardKey = "581efd9771149f70475fbd307d0744913d697976d63b98b7123ab97e111cefab";
+
+
+    private void Start()
+    {
+        GetLeaderboard();
+    }
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg)=> {
+            int loopLen = (msg.Length < names.Count) ? msg.Length : names.Count;
+            for (int i = 0; i < loopLen; ++i)
+            {
+                float min = Mathf.FloorToInt(msg[i].Score / 60);
+                float sec = Mathf.FloorToInt(msg[i].Score % 60);
+                scores[i].text = string.Format("{0:00} : {1:00}", min, sec);
+                names[i].text = msg[i].Username;
+            }
+        }));
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) => {
+            GetLeaderboard();
+        }));
+    }
+
+
+}
---
+++ WALL-B/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+    private int rawScore = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TMP_InputField inputUsername;
+
+    public UnityEvent<string, int> submitScoreEvent;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void SetScore(int score)
+    {
+        rawScore = score;
+
+        float min = Mathf.FloorToInt(rawScore / 60);
+        float sec = Mathf.FloorToInt(rawScore % 60);
+
+        scoreText.text = string.Format("{0:00} : {1:00}", min, sec);
+    }
+
+    public void SubmitScore()
+    {
+        submitScoreEvent.Invoke(inputUsername.text, rawScore);
+    }
+}
