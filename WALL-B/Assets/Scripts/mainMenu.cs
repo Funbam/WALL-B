b8030d91ec2a2ffcb5059665da232026fd3c5cdb
Reallife101 <ryan.ngoon@gmail.com>
2023-04-23 10:12:46 -0700

    enhance menu

--- WALL-B/Assets/Scripts/mainMenu.cs
+++ WALL-B/Assets/Scripts/mainMenu.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 
 public class mainMenu : MonoBehaviour
 {
+    [Header("Cinemachine Cam")]
     [SerializeField] private CinemachineVirtualCamera main;
     [SerializeField] private CinemachineVirtualCamera howToPlay;
     [SerializeField] private CinemachineVirtualCamera credits;
+
+    [Header("UI")]
+    [SerializeField] private GameObject mainUI;
+    [SerializeField] private GameObject howToPlayUI;
+    [SerializeField] private GameObject creditsUI;
 
     private void Start()
     {
@@ -19,6 +25,9 @@ public class mainMenu : MonoBehaviour
         howToPlay.Priority = 100;
         credits.Priority = -1;
         main.Priority = -1;
+        mainUI.SetActive(false);
+        howToPlayUI.SetActive(true);
+        creditsUI.SetActive(false);
     }
 
     public void mainMenuCam()
@@ -26,6 +35,9 @@ public class mainMenu : MonoBehaviour
         howToPlay.Priority = -1;
         credits.Priority = -1;
         main.Priority = 100;
+        mainUI.SetActive(true);
+        howToPlayUI.SetActive(false);
+        creditsUI.SetActive(false);
     }
 
     public void creditsCam()
@@ -33,6 +45,9 @@ public class mainMenu : MonoBehaviour
         howToPlay.Priority = -1;
         credits.Priority = 100;
         main.Priority = -1;
+        mainUI.SetActive(false);
+        howToPlayUI.SetActive(false);
+        creditsUI.SetActive(true);
     }
 
     public void quitGame()
