4dfd7c70a7499939a1422057dd6609ab496a93b4
Reallife101 <ryan.ngoon@gmail.com>
2023-04-23 01:20:42 -0700

    add pause

---
+++ WALL-B/Assets/Scripts/pauseGame.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class pauseGame : MonoBehaviour
+{
+    public GameObject pause;
+
+    public void togglePause()
+    {
+        if (pause.activeInHierarchy)
+        {
+            pause.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            showPause();
+            Time.timeScale = 0f;
+        }
+    }
+
+
+    public void showPause()
+    {
+        pause.SetActive(true);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            togglePause();
+        }
+    }
+}
