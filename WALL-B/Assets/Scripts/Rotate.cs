c2c2cdc51f9da3d933672267e668a2de2ceac302
Funbam <bambamtheram@gmail.com>
2023-04-23 14:46:41 -0700

    :DDDD

--- WALL-B/Assets/Scripts/Rotate.cs
+++ WALL-B/Assets/Scripts/Rotate.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    [SerializeField] private List<RectTransform> images;
+    [SerializeField] private float rotateTime;
+    private float timePassed = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,12 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timePassed += Time.deltaTime;
+        float rotation = Mathf.Cos(timePassed / rotateTime) * 10;
+        foreach(RectTransform item in images)
+        {
+            item.rotation = Quaternion.Euler(item.rotation.x, item.rotation.y, rotation);
+        }
+
     }
 }
